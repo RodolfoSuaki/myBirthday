01799efee3bea3364ff4a7b30b7e862a0ec5ce01
Rodolfo Suaki <rmsuaki@hotmail.com>
2023-08-25 00:30:31 -0300

    finalizando o projeto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-console.log("olá")
+AOS.init();
+
+//Constante da data do evento e o timestamp
+const dataDoEvento = new Date("Jul 12, 2024 20:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    diaEmMs = 1000 * 60 * 60 * 24;
+    horaEmMs = 1000 * 60 * 60;
+    minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0){
+        document.getElementById('contador').innerHTML = `Evento expirado`
+    }
+}, 1000);
